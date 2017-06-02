5215075ecaf17e82c227ad8b76c8b4d3399afc7e
petiorv <petiorv@gmail.com>
2017-06-02 16:48:00 +0300

    Matrices - Exercise
    2/12

---
+++ C# Advanced/Matrices - Exercise/01. Matrix of Palindromes/Startup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Matrix_of_Palindromes
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Regex.Split(Console.ReadLine(), " ").Select(int.Parse).ToArray();
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int rows = input[0];
+            int cols = input[1];
+            int cnt = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                
+                for (int col = 0; col < cols; col++)
+                {
+                    
+                    Console.Write($"{alphabet[row]}{alphabet[cnt+col]}{alphabet[row]} ");
+                    
+                }
+                cnt++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/Matrices - Exercise/02. Diagonal Diff/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02.Diagonal_Diff
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[,] matrix = new long[n, n];
+
+
+            for (int row = 0; row < n; row++)
+            {
+                long[] currentRow = Console.ReadLine()
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToArray();
+                for (int col = 0; col < n; col++)
+                {
+
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            long primeDiagonal = 0;
+            long secondDiagonal = 0;
+            int primeCnt = 0;
+            for (int row = 0; row < n; row++)
+            {
+
+                primeDiagonal += matrix[row, primeCnt];
+                primeCnt++;
+            }
+
+            int secondCnt = n-1;
+            for (int i = 0; i < n; i++)
+            {
+                secondDiagonal += matrix[i, secondCnt];
+                secondCnt--;
+            }
+            long diff = Math.Abs(primeDiagonal - secondDiagonal);
+            Console.WriteLine(diff);
+        }
+    }
+}
