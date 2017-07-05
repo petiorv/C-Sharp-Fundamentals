6dc49e0a2b9dfa8f2b7e36931d2ab78d212b8c64
petiorv <petiorv@gmail.com>
2017-07-05 16:55:59 +0300

    EncapsEX

---
+++ C# OOP Basic/EncapsulationExercise/ShoppingSpree/Person.cs
@@ -0,0 +1,65 @@
+﻿namespace ShoppingSpree
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Person
+    {
+        private string name;
+        private decimal money;
+
+        public List<Product> Bag { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        
+        public decimal Money
+        {
+            get
+            {
+                return this.money;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Bag = new List<Product>();
+        }
+
+        public void BuyProduct(Product prod)
+        {
+            if (this.money >= prod.Coast)
+            {
+                this.money -= prod.Coast;
+                this.Bag.Add(prod);
+            }
+            else
+            {
+                throw new ArgumentException($"{this.name} can't afford {prod.Name}");
+            }
+        }
+    }
+}
---
+++ C# OOP Basic/EncapsulationExercise/ShoppingSpree/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingSpree
+{
+    class Product
+    {
+        private string name;
+        private decimal coast;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Coast
+        {
+            get
+            {
+                return this.coast;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.coast = value;
+            }
+        }
+
+        public Product(string name, decimal coast)
+        {
+            this.Name = name;
+            this.Coast = coast;
+        }
+    }
+}
