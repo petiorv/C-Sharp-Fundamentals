7381fbc33e5bf311380c3fd50340ebea06b7e744
petiorv <petiorv@gmail.com>
2017-07-07 16:57:36 +0300

    Man kind in progress

---
+++ C# OOP Basic/InheritanceExercise/ManKindExercise/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Student : Human
+{
+    private string facultyNum;
+
+    public Student(string firstName, string lastName, string facultyNum)
+        : base (firstName, lastName)
+    {
+        this.FacultyNum = facultyNum;
+    }
+
+    public string FacultyNum
+    {
+        get
+        {
+            return this.facultyNum;
+        }
+        set
+        {
+            if (value.Length < 5 && value.Length > 10)
+            {
+                throw new ArgumentException("Invalid faculty number!");
+            }
+            this.facultyNum = value;
+        }
+    }
+}
---
+++ C# OOP Basic/InheritanceExercise/ManKindExercise/Worker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+public class Worker : Human
+{
+    private decimal weekSalary;
+    private double workHoursPerDay;
+    private decimal salaryPerHour;
+    
+    public Worker(string firstName, string lastName, decimal weekSalary, double hours) 
+        : base (firstName, lastName)
+    {
+        this.WeekSalary = weekSalary;
+    }
+
+    public 
+
+    public decimal WeekSalary
+    {
+        get
+        {
+            return this.weekSalary;
+        }
+        set
+        {
+            if (value < 10)
+            {
+                throw new ArgumentException("Expected value mismatch! Argument: weekSalary");
+            }
+            this.weekSalary = value;
+        }
+    }
+    public double WorkHoursPerDay
+    {
+        get
+        {
+            return this.workHoursPerDay;
+        }
+        set
+        {
+            if (value < 1 || value > 12)
+            {
+                throw new ArgumentException("Expected value mismatch! Argument: workHoursPerDay");
+            }
+            this.workHoursPerDay = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        var result = new StringBuilder();
+        result.AppendLine($"First Name: {this.FirstName}");
+        result.AppendLine($"Last Name: {this.LastName}");
+        result.AppendLine($"Week Salary: {this.WeekSalary}");
+        result.AppendLine($"Hours per day: {this.WorkHoursPerDay}");
+        result.AppendLine($"Salary per hour: {this.s}");
+        return "";
+    }
+}
+
