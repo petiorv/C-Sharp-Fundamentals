bf445daebd7891d0eda52c7a1448abd9b8dacee5
petiorv <petiorv@gmail.com>
2017-07-07 16:10:31 +0300

    Inheritance Exercise

---
+++ C# OOP Basic/InheritanceExercise/BookShopExercise/GoldenEditionBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook(string author, string title, decimal price)
+        : base (author, title, price)
+    {
+        
+    }
+
+    protected override decimal Price
+    {
+        get
+        {
+            return base.Price * 1.3m;
+        }
+    }
+}
