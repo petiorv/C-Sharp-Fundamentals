3c0a9f8b4229344bfd2e313054adf0cc2a8b78ec
petiorv <petiorv@gmail.com>
2017-07-07 16:57:52 +0300

    Man kind

--- C# OOP Basic/InheritanceExercise/ManKindExercise/Human.cs
+++ C# OOP Basic/InheritanceExercise/ManKindExercise/Human.cs
@@ -1,5 +1,51 @@
-﻿public class Human
-{
+﻿using System;
 
+public class Human
+{
+    private string firstName;
+    private string lastName;
+
+    public Human(string firstName, string lastName)
+    {
+
+    }
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+        set
+        {
+            if (char.IsLower(value[0]))
+            {
+                throw new ArgumentException("Expected upper case letter! Argument: firstName");
+            }
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Expected length at least 4 symbols! Argument: firstName");
+            }
+            this.firstName = value;
+        }
+    }
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        set
+        {
+            if (char.IsLower(value[0]))
+            {
+                throw new ArgumentException("Expected upper case letter! Argument: lastName");
+            }
+            if (value.Length < 2)
+            {
+                throw new ArgumentException("Expected length at least 3 symbols! Argument: lastName ");
+            }
+        }
+    }
 }
 
