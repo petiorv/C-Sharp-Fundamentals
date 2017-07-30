bd9de49e5b324c153cc6b8490b67371353f1509c
petiorv <petiorv@gmail.com>
2017-07-30 23:47:27 +0300

    4/4

--- C# OOP Advanced/IteratorsAndComparatorsLab/LibraryFM452/Book.cs
+++ C# OOP Advanced/IteratorsAndComparatorsLab/LibraryFM452/Book.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-public class Book
+public class Book : IComparable<Book>
 {
     public Book(string title, int year, params string[] authors)
     {
@@ -12,5 +13,21 @@ public class Book
     public string Title { get; set; }
     public int Year { get; set; }
     public IReadOnlyList<string> Authors { get; set; }
+
+    public int CompareTo(Book otherBook)
+    {
+        int result = this.Year.CompareTo(otherBook.Year);
+
+        if (result == 0)
+        {
+            result = this.Title.CompareTo(otherBook.Title);
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Year}";
+    }
 }
 
